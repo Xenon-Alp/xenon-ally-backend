71c8ef265cbd32843c854de578e53eada0233ab9
xenon-alp <accpitawala@gmail.com>
2026-05-06 22:16:17 +0530

    Fix Gmail SMTP port for Nodemailer

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ const app = express();
 const PORT = 3000;
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
